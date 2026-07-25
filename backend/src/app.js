7224d1c0519892759d8383ae52ef8baf2f659d42
meshivamguptaa <shivammgupta0408@gmail.com>
2026-07-25 18:31:38 +0530

    feat(database): Setup mongodb connection

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to QuillMind API",
+  });
+});
+
+export default app;
